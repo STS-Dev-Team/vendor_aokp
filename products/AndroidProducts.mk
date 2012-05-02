8d51ebcaeedb6396647b717e27a122a5d1dadc3a
dhacker29 <davidhackerdvm@gmail.com>
2012-05-02 05:14:10 -0500

    update for ics leak builds

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -7,25 +7,20 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/captivatemtd.mk \
     $(LOCAL_DIR)/fascinatemtd.mk \
     $(LOCAL_DIR)/galaxysmtd.mk \
-    $(LOCAL_DIR)/maserati.mk \
     $(LOCAL_DIR)/inc.mk \
     $(LOCAL_DIR)/otter.mk \
     $(LOCAL_DIR)/p1c.mk \
     $(LOCAL_DIR)/p4.mk \
     $(LOCAL_DIR)/p4vzw.mk \
     $(LOCAL_DIR)/p4wifi.mk \
-    $(LOCAL_DIR)/solana.mk \
     $(LOCAL_DIR)/spyder.mk \
     $(LOCAL_DIR)/p5wifi.mk \
     $(LOCAL_DIR)/p990.mk \
     $(LOCAL_DIR)/p999.mk \
     $(LOCAL_DIR)/picasso.mk \
     $(LOCAL_DIR)/pyramid.mk \
-    $(LOCAL_DIR)/stingray.mk \
     $(LOCAL_DIR)/supersonic.mk \
-    $(LOCAL_DIR)/targa.mk \
     $(LOCAL_DIR)/tenderloin.mk \
     $(LOCAL_DIR)/tf101.mk \
     $(LOCAL_DIR)/vibrantmtd.mk \
     $(LOCAL_DIR)/vivow.mk \
-    $(LOCAL_DIR)/wingray.mk \
